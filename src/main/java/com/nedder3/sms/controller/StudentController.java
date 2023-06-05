e992031f5aee172af781f768b32fd2a6539dda60
nedder3 <arij.dev@gmail.com>
2023-06-05 17:35:27 -0300

    Add getStudents implementation endpoint

--- src/main/java/com/nedder3/sms/controller/StudentController.java
+++ src/main/java/com/nedder3/sms/controller/StudentController.java
@@ -1,9 +1,26 @@
 package com.nedder3.sms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.nedder3.sms.service.StudentService;
 
 @Controller
 public class StudentController {
 	
+	private StudentService studentService;
+
+	public StudentController(StudentService studentService) {
+		super();
+		this.studentService = studentService;
+	}
+	
+	//handlder method to handle list student and return model and view
+	@GetMapping("/students")
+	public String listStudents(Model model) {
+		model.addAttribute("students",studentService.getAllStudents());
+		return "students";
+	}
 
 }
