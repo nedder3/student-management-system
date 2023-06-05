2eb4fd999d0c895df82eba6265ce0afb44b894ab
nedder3 <arij.dev@gmail.com>
2023-06-05 17:17:08 -0300

    Update implementation method

--- src/main/java/com/nedder3/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/nedder3/sms/service/impl/StudentServiceImpl.java
@@ -1,7 +1,20 @@
 package com.nedder3.sms.service.impl;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nedder3.sms.entity.Student;
 import com.nedder3.sms.service.StudentService;
 
+@Service
 public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public List<Student> getAllStudents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
