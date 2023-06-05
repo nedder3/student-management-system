0ab5d5d4f3e17f13ed69cbbaf9ad74c03d2436a3
nedder3 <arij.dev@gmail.com>
2023-06-05 14:57:39 -0300

    Add initial scaffolding

---
+++ src/test/java/com/nedder3/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nedder3.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
