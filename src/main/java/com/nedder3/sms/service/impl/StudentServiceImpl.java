bc9c4f8ec9f7b13ab454a072f0cd182d64b949ec
nedder3 <arij.dev@gmail.com>
2023-06-05 17:24:41 -0300

    Update with findAll method

--- src/main/java/com/nedder3/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/nedder3/sms/service/impl/StudentServiceImpl.java
@@ -5,15 +5,26 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.nedder3.sms.entity.Student;
+import com.nedder3.sms.repository.StudentRepository;
 import com.nedder3.sms.service.StudentService;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+	private StudentRepository studentRepository;
+	
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+
+
 	@Override
 	public List<Student> getAllStudents() {
 		// TODO Auto-generated method stub
-		return null;
+		return studentRepository.findAll();
 	}
 	
 
