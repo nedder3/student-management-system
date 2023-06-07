e17ced98cf7dd87327cd24cfdfb42ef469c5c07d
nedder3 <arij.dev@gmail.com>
2023-06-07 04:25:10 -0300

    Add update student methods implementation

--- src/main/java/com/nedder3/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/nedder3/sms/service/impl/StudentServiceImpl.java
@@ -34,6 +34,22 @@ public class StudentServiceImpl implements StudentService {
 		// TODO Auto-generated method stub
 		return studentRepository.save(student);
 	}
+
+
+
+	@Override
+	public Student getStudentById(Long id) {
+		// TODO Auto-generated method stub
+		return studentRepository.findById(id).get();
+	}
+
+
+
+	@Override
+	public Student updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return studentRepository.save(student);
+	}
 	
 
 }
